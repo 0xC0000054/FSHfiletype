49885ed5c9f042b306394bf3ba6d6d9242764715
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-05-31 00:24:21 +0000

    Initial Commit

---
+++ FshFileFormat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FSHfiletype
+{
+    internal enum FshFileFormat 
+    {
+        TwentyFourBit,
+        ThirtyTwoBit,
+        DXT1,
+        DXT3
+    }
+
+
+}
