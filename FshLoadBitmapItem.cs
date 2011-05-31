814ecc07d34af6bea9c68e1a0ed38ebffacd8322
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-05-31 23:45:24 +0000

    Initial Commit

---
+++ FshLoadBitmapItem.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PaintDotNet;
+
+namespace FSHfiletype
+{
+    class FshLoadBitmapItem : IDisposable
+    {
+        private Surface surface;
+        private FshFileFormat format;
+
+        public Surface Surface
+        {
+            get
+            {
+                return surface;
+            }
+            internal set
+            {
+                surface = value;
+            }
+        }
+
+        public FshFileFormat Format
+        {
+            get
+            {
+                return format;
+            }
+            internal set
+            {
+                format = value;
+            }
+        }
+
+        public FshLoadBitmapItem()
+        {
+            this.disposed = false;
+            this.surface = null;
+            this.format = FshFileFormat.DXT1;
+        }
+
+        public FshLoadBitmapItem(int width, int height, FshFileFormat format) : this()
+        {
+            this.surface = new Surface(width, height);
+            this.format = format;
+        }
+
+        private bool disposed;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (surface != null)
+                    {
+                        surface.Dispose();
+                        surface = null;
+                    }
+                    disposed = true;
+                }
+            }
+        }
+
+    }
+}
