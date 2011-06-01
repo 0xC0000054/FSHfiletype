0da717cca478e9c9cdabaedf83b3478d6e9e19b2
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-06-01 03:34:21 +0000

    Minor code updates

--- Fshwrite.cs
+++ Fshwrite.cs
@@ -19,126 +19,7 @@ namespace FSHfiletype
             codelist = new List<int>();
             headdir = null;
         }
-        private enum SquishCompFlags
-        {
-            //! Use DXT1 compression.
-            kDxt1 = (1 << 0),
 
-            //! Use DXT3 compression.
-            kDxt3 = (1 << 1),
-
-            //! Use DXT5 compression.
-            kDxt5 = (1 << 2),
-
-            //! Use a very slow but very high quality colour compressor.
-            kColourIterativeClusterFit = (1 << 8),
-
-            //! Use a slow but high quality colour compressor (the default).
-            kColourClusterFit = (1 << 3),
-
-            //! Use a fast but low quality colour compressor.
-            kColourRangeFit = (1 << 4),
-
-            //! Use a perceptual metric for colour error (the default).
-            kColourMetricPerceptual = (1 << 5),
-
-            //! Use a uniform metric for colour error.
-            kColourMetricUniform = (1 << 6),
-
-        }
-       
-        private byte[] CompressImage(Bitmap image, int flags)
-        {
-            byte[] pixelData = new byte[image.Width * image.Height * 4];
-
-
-            BitmapData data = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-            try
-            {
-                unsafe
-                {
-                    for (int y = 0; y < image.Height; y++)
-                    {
-                        for (int x = 0; x < image.Width; x++)
-                        {
-                            byte* p = (byte*)data.Scan0.ToPointer() + (y * data.Stride) + (x * 4);
-                            int Offset = (y * image.Width * 4) + (x * 4);
-
-                            pixelData[Offset] = p[2];
-                            pixelData[Offset + 1] = p[1];
-                            pixelData[Offset + 2] = p[0];
-                            pixelData[Offset + 3] = p[3];
-                        }
-                    } 
-                }
-            }
-            finally
-            {
-                image.UnlockBits(data);
-            }
-
-            // Compute size of compressed block area, and allocate 
-            int blockCount = ((image.Width + 3) / 4) * ((image.Height + 3) / 4);
-            int blockSize = ((flags & (int)SquishCompFlags.kDxt1) != 0) ? 8 : 16;
-
-            // Allocate room for compressed blocks
-            byte[] blockData = new byte[blockCount * blockSize];
-
-            // Invoke squish::CompressImage() with the required parameters
-            CompressImageWrapper(pixelData, image.Width, image.Height, blockData, flags);
-
-            // Return our block data to caller..
-            return blockData;
-        }
-        private static bool Is64bit()
-        {
-            return IntPtr.Size == 8 ? true : false;
-        }
-        public void InitilizeSquish()
-        {
-            if (Is64bit())
-            {
-                Squish_64.SquishInitialize();
-            }
-            else
-            {
-                Squish_32.SquishInitialize();
-            }
-        }
-        private sealed class Squish_32
-        {
-            [DllImport(@"Native.x86\PaintDotNet.Native.x86.dll")]
-            public static extern unsafe void SquishCompressImage(byte* rgba, int width, int height, byte* blocks, int flags, [MarshalAs(UnmanagedType.FunctionPtr)]  ProgressFn pf);
-
-            [DllImport(@"Native.x86\PaintDotNet.Native.x86.dll")]
-            public static extern void SquishInitialize();
-        }
-        private delegate void ProgressFn(int workDone, int workTotal);
-
-        private sealed class Squish_64
-        {
-            [DllImport(@"Native.x64\PaintDotNet.Native.x64.dll")]
-            public static extern unsafe void SquishCompressImage(byte* rgba, int width, int height, byte* blocks, int flags, [MarshalAs(UnmanagedType.FunctionPtr)]  ProgressFn pf);
-            [DllImport(@"Native.x64\PaintDotNet.Native.x64.dll")]
-            public static extern void SquishInitialize();
-        }
-        private static unsafe void CompressImageWrapper(byte[] rgba, int width, int height, byte[] blocks, int flags)
-        {
-            fixed (byte* RGBA = rgba)
-            {
-                fixed (byte* Blocks = blocks)
-                {
-                    if (Is64bit())
-                    {
-                        Squish_64.SquishCompressImage(RGBA, width, height, Blocks, flags, null);
-                    }
-                    else
-                    {
-                        Squish_32.SquishCompressImage(RGBA, width, height, Blocks, flags, null);
-                    }
-                }
-            }
-        }
         private Bitmap BlendDXTBmp(Bitmap colorbmp, Bitmap bmpalpha, bool dxt1)
         {
             Bitmap image = null;
@@ -159,11 +40,6 @@ namespace FSHfiletype
             { 
                 colorbmp = colorbmp.Clone(new Rectangle(0, 0, colorbmp.Width, colorbmp.Height), PixelFormat.Format32bppArgb);
             }
-
-            /*if (bmpalpha.PixelFormat != PixelFormat.Format32bppArgb)
-            {
-                bmpalpha = bmpalpha.Clone(new Rectangle(0, 0, bmpalpha.Width, bmpalpha.Height), PixelFormat.Format32bppArgb);
-            }*/
 
             BitmapData colordata = colorbmp.LockBits(new Rectangle(0, 0, colorbmp.Width, colorbmp.Height), ImageLockMode.ReadOnly, colorbmp.PixelFormat);
             BitmapData alphadata = bmpalpha.LockBits(new Rectangle(0, 0, bmpalpha.Width, bmpalpha.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
@@ -332,10 +208,10 @@ namespace FSHfiletype
                             Bitmap temp = BlendDXTBmp(bmp, alpha, true);
                             byte[] data = new byte[temp.Width * temp.Height * 4];
                             int flags = 0;
-                            flags |= (int)SquishCompFlags.kDxt1;
-                            flags |= (int)SquishCompFlags.kColourIterativeClusterFit;
-                            flags |= (int)SquishCompFlags.kColourMetricPerceptual;
-                            data = CompressImage(temp, flags);
+                            flags |= (int)Squish.SquishFlags.kDxt1;
+                            flags |= (int)Squish.SquishFlags.kColourIterativeClusterFit;
+                            flags |= (int)Squish.SquishFlags.kColourMetricPerceptual;
+                            data = Squish.CompressImage(temp, flags);
                             ms.Write(data, 0, data.Length);
                         }
                         else if (code == 0x61) // DXT3
@@ -343,10 +219,10 @@ namespace FSHfiletype
                             Bitmap temp = BlendDXTBmp(bmp, alpha, false);
                             byte[] data = new byte[temp.Width * temp.Height * 4];
                             int flags = 0;
-                            flags |= (int)SquishCompFlags.kDxt3;
-                            flags |= (int)SquishCompFlags.kColourIterativeClusterFit;
-                            flags |= (int)SquishCompFlags.kColourMetricPerceptual;
-                            data = CompressImage(temp, flags);
+                            flags |= (int)Squish.SquishFlags.kDxt3;
+                            flags |= (int)Squish.SquishFlags.kColourIterativeClusterFit;
+                            flags |= (int)Squish.SquishFlags.kColourMetricPerceptual;
+                            data = Squish.CompressImage(temp, flags);
                             ms.Write(data, 0, data.Length);
                         }
 
