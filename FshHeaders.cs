5b53f125d1a73f57b379edd82e5edafbfff1bbad
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2014-07-28 06:16:11 +0000

    Changed all structs to classes.

--- FshHeaders.cs
+++ FshHeaders.cs
@@ -2,20 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace FSHfiletype
 {
-    internal struct FSHHeader
+    internal sealed class FSHHeader
     {
-        public byte[] SHPI;
-        public int size;
-        public int numBmps;
-        public byte[] dirID;
+        private int size;
+        private int imageCount;
+        private byte[] dirID;
 
-        internal const int SizeOf = 16; 
+        /// <summary>
+        /// The FSH file signature - SHPI
+        /// </summary>
+        private const uint FSHSignature = 0x49504853U; 
+        internal const int SizeOf = 16;
+
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+        
+        public int ImageCount
+        {
+            get
+            {
+                return imageCount;
+            }
+        }
+
+        public FSHHeader(Stream stream)
+        {
+            if (stream.ReadUInt32() != FSHSignature)
+            {
+                throw new FileFormatException(Properties.Resources.InvalidFshHeader);
+            }
+            this.size = stream.ReadInt32();
+            this.imageCount = stream.ReadInt32();
+            this.dirID = new byte[4];
+            stream.ProperRead(this.dirID, 0, 4);
+        }
+
+        public FSHHeader(int imageCount, byte[] dirID)
+        {
+            this.size = 0;
+            this.imageCount = imageCount;
+            this.dirID = dirID;
+        }
+
+        public void Save(Stream stream)
+        {
+            stream.WriteUInt32(FSHSignature);
+            stream.WriteInt32(this.size);
+            stream.WriteInt32(this.imageCount);
+            stream.Write(this.dirID, 0, 4);
+        }
     }
 
-    internal struct FSHDirEntry
+    internal sealed class FSHDirEntry
     {
         public byte[] name;
         public int offset;
@@ -28,10 +79,22 @@ namespace FSHfiletype
             this.offset = 0;
         }
 
+        internal FSHDirEntry(Stream input)
+        {
+            this.name = new byte[4];
+            input.ProperRead(name, 0, 4);
+            this.offset = input.ReadInt32();
+        }
+
+        public void Save(Stream stream)
+        {
+            stream.Write(this.name, 0, 4);
+            stream.WriteInt32(this.offset);
+        }
     }
 
     [Serializable]
-    internal struct FSHEntryHeader
+    internal sealed class FSHEntryHeader
     {
         public int code;
         public ushort width;
@@ -41,7 +104,15 @@ namespace FSHfiletype
         [NonSerialized]
         internal const int SizeOf = 16;
 
-        internal FSHEntryHeader(System.IO.Stream stream)
+        public FSHEntryHeader()
+        {
+            this.code = 0;
+            this.width = 0;
+            this.height = 0;
+            this.misc = new ushort[4];
+        }
+
+        internal FSHEntryHeader(Stream stream)
         {
             this.code = stream.ReadInt32();
             this.width = stream.ReadUInt16();
