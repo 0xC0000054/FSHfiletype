f5f8ca59c3b7d1b850d7034cf9eb10f202d81de4
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2014-07-28 08:11:09 +0000

    Added ReadUInt32 and WriteUInt32 methods.

--- StreamExtensions.cs
+++ StreamExtensions.cs
@@ -4,7 +4,6 @@ namespace FSHfiletype
 {
     static class StreamExtensions
     {
-
         public static int ReadInt32(this Stream s)
         {
             int byte0 = s.ReadByte();
@@ -35,6 +34,36 @@ namespace FSHfiletype
             return (int)((byte3 << 24) | (byte2 << 16) | (byte1 << 8) | byte0);
         }
 
+        public static uint ReadUInt32(this Stream s)
+        {
+            int byte0 = s.ReadByte();
+
+            if (byte0 == -1)
+            {
+                throw new EndOfStreamException();
+            }
+            int byte1 = s.ReadByte();
+
+            if (byte1 == -1)
+            {
+                throw new EndOfStreamException();
+            }
+            int byte2 = s.ReadByte();
+
+            if (byte2 == -1)
+            {
+                throw new EndOfStreamException();
+            }
+            int byte3 = s.ReadByte();
+
+            if (byte3 == -1)
+            {
+                throw new EndOfStreamException();
+            }
+
+            return (uint)((byte3 << 24) | (byte2 << 16) | (byte1 << 8) | byte0);
+        }
+
         public static ushort ReadUInt16(this Stream s)
         {
             int byte0 = s.ReadByte();
@@ -57,6 +86,14 @@ namespace FSHfiletype
         {
             s.WriteByte((byte)(value & 0xff));
             s.WriteByte((byte)((value >> 8) & 0xff));
+        }
+
+        public static void WriteUInt32(this Stream s, uint value)
+        {
+            s.WriteByte((byte)(value & 0xff));
+            s.WriteByte((byte)((value >> 8) & 0xff));
+            s.WriteByte((byte)((value >> 16) & 0xff));
+            s.WriteByte((byte)((value >> 24) & 0xff));
         }
 
         public static void WriteInt32(this Stream s, int value)
